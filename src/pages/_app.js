56aa5bf9806efb0ead873ed458da36ed6c16b4ce
vijay chowdary <vijaychowdary3699@gmail.com>
2021-09-18 17:33:32 +0530

    favicon+titile

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,13 @@
+import Head from 'next/head';
 import Theme from '../styles/theme';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>VIJAY-CN</title>
+        <link rel="icon" type="image/png" href="https://avatars.dicebear.com/api/male/vijay.svg" />
+      </Head>
       <Theme>
         <Component {...pageProps} />
       </Theme>
