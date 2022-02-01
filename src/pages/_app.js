c52eb2bf3f497965c340462fa922e04fadcab781
V1ZEI-CN <vijaychowdary3699@gmail.com>
2022-02-01 18:44:47 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>VIJAY-CN</title>
+        <title>V1JA! CN</title>
         <link rel="icon" type="image/png" href="https://avatars.dicebear.com/api/male/vijay.svg" />
       </Head>
       <Theme>
